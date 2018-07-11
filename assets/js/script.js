db387607c1546be439da6fe5de8a93ad9fa0f00b
cagrioz <cagriozarpaciii@hotmail.com>
2018-07-11 11:25:44 +0300

    Extra Pages and Modules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 jQuery(function($) {
     "use strict";
 
-    $(".rache-featured .featured-carousel").owlCarousel({
+    /* =========== Featured Slider =========== */
+    $(".featured-slider").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         loop: true,
@@ -12,13 +13,64 @@ jQuery(function($) {
         responsive: {
             0: {
                 items: 1,
-                nav: false
+                nav: false,
+                margin: 0
             },
-            950:{ 
+            992:{ 
                 items: 2,
                 nav: true
-            }
+            },
         }
     });
+
+    /* =========== Featured Carousel =========== */
+    $(".featured-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        dots: false,
+        nav: false,
+        margin: 10,
+        center: true,
+        responsive: {
+            0:{ items: 1, margin: 0 },
+            768:{ items: 3 },
+            1200:{ items: 5 },
+        }
+    });
+
+    /* =========== Gallery Slider =========== */
+    $(".gallery-slider").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: false,
+        margin: 0
+    });
+
+    /* =========== Responsive Menu =========== */
+    $(".menu-btn").on("click", function() {
+        $(".sidemenu").toggleClass("slidein"); 
+        $(".wrapper").toggleClass("stop"); 
+    });
+    
+    /* =========== Sidemenu Dropdown =========== */
+    $(".mobile-menu ul").parent().addClass("menu-item-has-children");
+    $(".mobile-menu li.menu-item-has-children > a").on("click",function(){
+        $(this).next("ul").slideToggle();
+        $(this).parent().siblings().find("ul").slideUp();
+        return false;
+    });
+
+    /* =========== Mobile Navigation Dropdown =========== */
+    $(".search-btn").on("click", function(e) {
+        e.preventDefault();
+        $(".search-popup").toggleClass("search-active");
+        $('.search-popup input[type="search"]').focus();
+    });
+
 
 });
